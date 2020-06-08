e607bbf33385389a8eb5cbf236cecdb3af93fd80
Wojtek <wowaked@gmail.com>
2020-06-08 16:30:46 +0200

    debugging

--- certs/letsEncrypt.sh
+++ certs/letsEncrypt.sh
@@ -14,22 +14,23 @@ echo "domain: " $domain
 echo "email: " $email
 
 #if [[ 1 -lt 0 ]]  ; then 
-echo 2 | \
-certbot certonly \
-            --logs-dir /opt/certbot/logs \
-            --config-dir /opt/certbot/config \
-            --work-dir /opt/certbot/work  \
-            --manual-auth-hook ./authenticator.sh \
-            --manual-cleanup-hook ./cleanup.sh \
-            --manual-public-ip-logging-ok \
-            --agree-tos  \
-            --preferred-challenges dns \
-            --manual \
-            -m $email \
-            --domains "*.$domain, $domain" && {
+#echo 2 | \
+#certbot certonly \
+#            --logs-dir /home/wojtek/Documents/certbot/logs \
+ #           --config-dir /home/wojtek/Documents/certbot/config \
+ #           --work-dir /home/wojtek/Documents/certbot/work  \
+  #          --manual-auth-hook ./authenticator.sh \
+    #        --manual-cleanup-hook ./cleanup.sh \
+      #      --manual-public-ip-logging-ok \
+        #    --agree-tos  \
+          #  --preferred-challenges dns \
+           # --manual \
+            #-m $email \
+            #--domains "*.$domain, $domain" && {
                     # Create or update certificate in ACM 
-                    python3 updateCertManager.py $domain && {
+               #     python3 updateCertManager.py $domain && {
                             # Upload certificate to EC2
+                            echo $AWS_SSH_KEY
                             scp -i $AWS_SSH_KEY /opt/certbot/config/live/$domain/cert.pem  ec2-user@backend.$domain:
                             scp -i $AWS_SSH_KEY /opt/certbot/config/live/$domain/fullchain.pem  ec2-user@backend.$domain:
                             scp -i $AWS_SSH_KEY /opt/certbot/config/live/$domain/privkey.pem ec2-user@backend.$domain:
@@ -37,14 +38,14 @@ certbot certonly \
                             # Restart httpd
                             ssh -i $AWS_SSH_KEY ec2-user@backend.$domain "sudo service httpd restart"
                             ssh -i $AWS_SSH_KEY ec2-user@backend.$domain "sudo service httpd status"
-                    } || {
-                        echo "Something went wrong"
-                        exit 1
-                    }
-            } || {
-                echo "Something went wrong"
-                exit 1
-            }
+                  #  } || {
+                     #   echo "Something went wrong"
+                        #exit 1
+                    #}
+           # } || {
+             #   echo "Something went wrong"
+               # exit 1
+           # }
             
 #fi            
 
