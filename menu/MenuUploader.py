4e4b8cbef3e2797a5bd28ae5bcaf9b4fa5630691
Wojtek <wowaked@gmail.com>
2020-06-10 16:28:57 +0200

    fix up returns

--- menu/MenuUploader.py
+++ menu/MenuUploader.py
@@ -12,20 +12,15 @@ from imap_tools import MailBox
 
 MENU_FILE = "menu.pdf"
 
+def returnMessage(body, status=200):
+    return {
+        'statusCode': status,
+        'body': response,
+    }
+    
 def addAttachmentToS3(message, session):
     s3client = session.client("s3")
     cfClient = session.client("cloudfront")
-
-    if(len(message.attachments) == 0 ):
-        print(f"No attachments in this message. {message.text}")
-    
-    print(f"Attachment found. filename is: {message.attachments[0].filename}")
-    
-    if(not message.attachments[0].filename.endswith(".pdf")):
-        print("File not a pdf, so not publishing it")
-        return {
-            'statusCode': 400,
-        }
 
     resp = s3client.put_object(
             Body=message.attachments[0].payload, Bucket=os.environ["BUCKET_NAME"], Key=MENU_FILE, 
@@ -58,13 +53,21 @@ def updateMenu(session):
     with MailBox('imap.gmail.com').login(email_user,email_pass, 'INBOX') as mailbox:
         for message in mailbox.fetch(limit=1, reverse=True):
 
+            if(len(message.attachments) == 0 ):
+                 return returnMessage("No attachments in this message", "400")
+            
+            print(f"Attachment found. filename is: {message.attachments[0].filename}")
+            
+            if(not message.attachments[0].filename.endswith(".pdf")):
+                return returnMessage("File not a pdf, so not publishing it", "400")
+    
             try :
                 resp = s3client.get_object_tagging(
                         Bucket=os.environ["BUCKET_NAME"],
                          Key=MENU_FILE,
                     )
             except: 
-                addAttachmentToS3(message, session)
+                ddAttachmentToS3(message, session)
             
             if(len(resp['TagSet']) == 0) :
                #Found a menu, but it';s not been tagged so can't really tell how old it is.  Treating it as if we didn't have one. 
@@ -73,23 +76,17 @@ def updateMenu(session):
             existingMenuDate = resp['TagSet'][0]['Value']
             
             if(float(existingMenuDate) == message.date.timestamp()):
-                print("This menu has been uploaded from this message.")
+                return "The current menu has been uploaded from this message."
             elif(float(existingMenuDate) < message.date.timestamp()):
-                print("'Email is newer so upload")
+                print("Email is newer so upload")
                 addAttachmentToS3(message, session)
+                return returnMessage("Email is newer and menu has been uploaded", 200)
             else:
-                print(f"'This shouldn't happen: {existingMenuDate}")
-                return {
-                    'statusCode': 400,
-                }
+                return returnMessage(f"'This shouldn't happen: {existingMenuDate}", "400")
 
 def lambda_handler(event, context):
     session = boto3.Session()
-    updateMenu(session)
-
-    return {
-        'statusCode': 200,
-    }
+    return updateMenu(session)
     
 if __name__ == "__main__":
     session = boto3.Session(profile_name="wpuser")
