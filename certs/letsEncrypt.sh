ce50783bd979348f2a6a64d007abe44bbcbc1a37
Wojtek <wowaked@gmail.com>
2021-10-12 08:36:43 +0200

    update cert path
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- certs/letsEncrypt.sh
+++ certs/letsEncrypt.sh
@@ -13,18 +13,17 @@ echo "skipcert: " $skipCert_flag
 echo "domain: " $domain
 echo "email: " $email
 
-
-if [[ -z $skipCert_flag || $skipCert_flag == false ]] ; then
+if [[ -z $skipCert_flag ]] ; then
     echo 2 | \
         certbot certonly \
             --config-dir /opt/certbot/config \
-            --logs-dir     /opt/certbot/logs \
-            --work-dir   /opt/certbot/work  \
+            --logs-dir /opt/certbot/logs \
+            --work-dir /opt/certbot/work  \
             --agree-tos  \
             --domains "*.$domain, $domain" \
             -m $email \
             --manual \
-            --manual-auth-hook       ./authenticator.sh \
+            --manual-auth-hook ./authenticator.sh \
             --manual-cleanup-hook ./cleanup.sh \
             --manual-public-ip-logging-ok \
             --preferred-challenges dns && {
@@ -38,9 +37,9 @@ fi
 python3 updateCertManager.py $domain && {
     # Upload certificate to EC2
     echo "Start copying keys..."
-    scp -oStrictHostKeyChecking=no  -i $AWS_SSH_KEY /opt/certbot/config/live/$domain/cert.pem  ec2-user@backend.$domain: 
-    scp -oStrictHostKeyChecking=no  -i $AWS_SSH_KEY /opt/certbot/config/live/$domain/fullchain.pem  ec2-user@backend.$domain:
-    scp -oStrictHostKeyChecking=no  -i $AWS_SSH_KEY /opt/certbot/config/live/$domain/privkey.pem ec2-user@backend.$domain: 
+    scp -oStrictHostKeyChecking=no  -i $AWS_SSH_KEY /opt/certbot/config/live/$domain/cert.pem  ec2-user@backend.$domain:$domain
+    scp -oStrictHostKeyChecking=no  -i $AWS_SSH_KEY /opt/certbot/config/live/$domain/fullchain.pem  ec2-user@backend.$domain:$domain
+    scp -oStrictHostKeyChecking=no  -i $AWS_SSH_KEY /opt/certbot/config/live/$domain/privkey.pem ec2-user@backend.$domain:$domain
     echo "Finished copying keys."                        
     # Restart httpd
     ssh -oStrictHostKeyChecking=no -i $AWS_SSH_KEY ec2-user@backend.$domain "sudo service httpd restart"
@@ -49,7 +48,6 @@ python3 updateCertManager.py $domain && {
     echo "Something went wrong"
     exit 1
 }
-
             
 #TODO have to do this before we do anything!
 #  sudo systemd-resolve --flush-caches
