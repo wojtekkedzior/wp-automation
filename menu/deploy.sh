43d31ccc8387f4b838442dfdecdf74bc24b2803a
Wojtek <wowaked@gmail.com>
2022-10-17 14:57:18 +0200

    .
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- menu/deploy.sh
+++ menu/deploy.sh
@@ -1,3 +1,5 @@
-# email comes from a bucket where the email arrive
+pip3 install --upgrade  --system --target ./python  boto3
 
+# email comes from a bucket where the email arrive
 rm lambda.zip && zip -r lambda.zip * && aws --profile wpuser --region eu-west-1 lambda update-function-code --function-name menuUpdater --zip-file fileb://lambda.zip && sleep 10 && aws --profile wpuser s3 cp email s3://inbound-menu/$(uuid)
+
