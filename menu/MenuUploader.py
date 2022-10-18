c85905d3436a7b2793bd83239454089dd55f8ab8
Wojtek <wowaked@gmail.com>
2022-10-18 14:09:18 +0200

    .
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- menu/MenuUploader.py
+++ menu/MenuUploader.py
@@ -12,19 +12,10 @@ def lambda_handler(event, context):
 
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
-    print("bucket: ", bucket, " key: ", key)
-
     obj = s3.Object(bucket, key)
 
-    b = email.message_from_string(obj.get()['Body'].read().decode('utf-8') )
-    body = ""
-
-    for part in b.walk():
+    for part in email.message_from_string(obj.get()['Body'].read().decode('utf-8') ).walk():
         ctype = part.get_content_type()
         if ctype == 'application/pdf':
-            body = part.get_payload(decode=True)  # decode
+            s3.Object("erawan-menu", 'menu.pdf').put(Body=part.get_payload(decode=True))
             break
-
-    o = s3.Object("erawan-menu", key+'.pdf') # TODO this needs to be renamed to menu.pdf
-    o.put(Body=body)
-    return "yay"
