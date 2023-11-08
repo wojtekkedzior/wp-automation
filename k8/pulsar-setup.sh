08c3d80e662d4a677c487d35e0b3626a4e4aeab2
Wojtek <wowaked@gmail.com>
2023-11-08 13:59:58 +0100

    let replication create the topics on the backup
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- k8/pulsar-setup.sh
+++ k8/pulsar-setup.sh
@@ -43,21 +43,21 @@ function multiCluster() {
   kubectl exec -i pulsar-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin namespaces set-clusters wojtekt/wojtekns --clusters pulsar,plite2"
   # kubectl exec -i pulsar-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin topics create-partitioned-topic wojtekt/wojtekns/wojtektopic1 -p 2"
 
+  # on plite2: 
+  echo "setting up cluster on plite2" 
+  kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin tenants    create wojtekt --admin-roles my-admin-role --allowed-clusters pulsar,plite2"
+  kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin namespaces create wojtekt/wojtekns --bundles 4"
+  # kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin topics     create-partitioned-topic wojtekt/wojtekns/wojtektopic1 -p 2"
+  echo "cluster on plite2 is ready"
+
   createTestTopics "pulsar-toolset-0" 2
   sleep 5
   kubectl exec -i "pulsar-toolset-0" -- /bin/bash -c "/pulsar/bin/pulsar-admin topics create wojtekt/wojtekns/sun"
   sleep 5
   echo "cluster "pulsar" is ready"
 
-  # on plite2: 
-  echo "setting up cluster on plite2" 
-  kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin tenants    create wojtekt --admin-roles my-admin-role --allowed-clusters pulsar,plite2"
-  kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin namespaces create wojtekt/wojtekns --bundles 4"
-  # kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin topics     create-partitioned-topic wojtekt/wojtekns/wojtektopic1 -p 2"
-
-  createTestTopics "plite2-toolset-0" 2
+  # createTestTopics "plite2-toolset-0" 2
   # kubectl exec -i "plite2-toolset-0" -- /bin/bash -c "/pulsar/bin/pulsar-admin topics create wojtekt/wojtekns/sun"
-  echo "cluster on plite2 is ready"
 }
 
 function singleCluster {
