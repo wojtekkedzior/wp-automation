140bd55a4246eae07c92b64362fcf401f0282d93
Wojtek <wowaked@gmail.com>
2023-11-09 10:13:14 +0100

    rejig again
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- k8/pulsar-setup.sh
+++ k8/pulsar-setup.sh
@@ -47,11 +47,14 @@ function multiCluster() {
   kubectl exec -i pulsar-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin namespaces set-clusters wojtekt/wojtekns --clusters pulsar,plite2"
   echo "cluster "pulsar" is ready"
 
+  kubectl exec -i "pulsar-toolset-0" -- /bin/bash -c "/pulsar/bin/pulsar-admin topics create wojtekt/wojtekns/sun"
+   sleep 5 
+
   # on plite2: 
-  echo "setting up cluster on plite2" 
-  kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin tenants    create wojtekt --admin-roles my-admin-role --allowed-clusters pulsar,plite2"
-  kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin namespaces create wojtekt/wojtekns --bundles 4"
-  echo "cluster on plite2 is ready"
+  # echo "setting up cluster on plite2" 
+  # kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin tenants    create wojtekt --admin-roles my-admin-role --allowed-clusters pulsar,plite2"
+  # kubectl exec -i plite2-toolset-0  -- /bin/bash -c "/pulsar/bin/pulsar-admin namespaces create wojtekt/wojtekns --bundles 4"
+  # echo "cluster on plite2 is ready"
 
   # createTestTopics "plite2-toolset-0" 2
   # kubectl exec -i "plite2-toolset-0" -- /bin/bash -c "/pulsar/bin/pulsar-admin topics create wojtekt/wojtekns/sun"
