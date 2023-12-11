37e7a8526d520adfdb6d786351466596d2d2be1e
Wojtek <wowaked@gmail.com>
2023-12-11 18:01:53 +0100

    comments
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- k8/setup/tools.sh
+++ k8/setup/tools.sh
@@ -17,6 +17,8 @@ function hpa() {
 }
 
 function hazelcast() {
+    # helm repo add hazelcast https://hazelcast-charts.s3.amazonaws.com/
+    
     helm upgrade --install my-release hazelcast/hazelcast -f hazelcast-values.yaml #--set cluster.memberCount=3
     sleep 2
     sudo sed -i "/setenv HAZELCAST_IP/c\\\tsetenv HAZELCAST_IP $(kubectl get svc my-release-hazelcast -o json | jq -r '.spec.clusterIP')" /etc/haproxy/haproxy.cfg
