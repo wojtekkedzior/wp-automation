477bb51df99acb50d2fe028b0ac3ce78a53b3587
Wojtek <wowaked@gmail.com>
2023-12-20 13:49:35 +0100

    refactor create volumes
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- k8/create-volumes.sh
+++ k8/create-volumes.sh
@@ -1,5 +1,49 @@
 #!/bin/bash
 
+function multiCluster() {
+    path=$1
+    workers_from=$2
+    workers_to=$3
+    index=$4
+
+    for i in `eval echo {$workers_from..$workers_to}`;
+    do
+        for j in `eval echo {1..$index}`;
+        do
+            echo "$path/worker-$i-$j.qcow2"
+            qemu-img create "$path/worker-$i-$j.qcow2" 1G -f qcow2 -o preallocation=full
+        done
+
+        for j in `eval echo {$index..15}`;
+        do
+            echo "$path/worker-$i-$j.qcow2"
+            qemu-img create "$path/worker-$i-$j.qcow2" 40G -f qcow2 -o preallocation=full
+        done
+    done
+}
+
+function singleCluster() {
+    path=$1
+    workers_from=$2
+    workers_to=$3
+    index=$4
+
+    for i in `eval echo {$workers_from..$workers_to}`;
+    do
+        for j in `eval echo {1..$index}`;
+        do
+            echo "$path/worker-$i-$j.qcow2"
+            qemu-img create "$path/worker-$i-$j.qcow2" 2G -f qcow2 -o preallocation=full
+        done
+
+        for j in `eval echo {$index..15}`;
+        do
+            echo "$path/worker-$i-$j.qcow2"
+            qemu-img create "$path/worker-$i-$j.qcow2" 100G -f qcow2 -o preallocation=full
+        done
+    done
+}
+
 function create() {
     path=$1
     workers_from=$2
@@ -21,5 +65,6 @@ function create() {
         done
     done
 }
+
 
 create $1 $2 $3 $4
