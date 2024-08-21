baa7ce3d028ec3decd6fa84ab90efeea99e83f44
Wojtek <wowaked@gmail.com>
2024-08-21 11:52:04 +0200

    remove email related code
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- menu/deploy.sh
+++
@@ -1,5 +0,0 @@
-pip3 install --upgrade  --system --target ./python  boto3
-
-# email comes from a bucket where the email arrives
-rm lambda.zip && zip -r lambda.zip * && aws --profile wpuser --region eu-west-1 lambda update-function-code --function-name menuUpdater --zip-file fileb://lambda.zip && sleep 10 && aws --profile wpuser s3 cp email s3://inbound-menu/$(uuid)
-
