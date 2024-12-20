28981dffe79fc0356a4db67ebcdcadb50a5dcb8c
Wojtek <wowaked@gmail.com>
2024-12-20 07:52:44 +0100

    clean up and resize
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- k8/create-volumes.sh
+++ k8/create-volumes.sh
@@ -70,12 +70,13 @@ function staticVolumes() {
 
     path="/mnt/k8volumes"
     smallVolSz="10G"
+    verySmallVolSz="1G"
 
     #worker 1
         #journals
-        qemu-img create "${path}/worker1-1.qcow2" 75G -f qcow2 -o preallocation=full
-        qemu-img create "${path}/worker1-2.qcow2" 75G -f qcow2 -o preallocation=full
-        #journals
+        qemu-img create "${path}/worker1-1.qcow2" 70G -f qcow2 -o preallocation=full
+        qemu-img create "${path}/worker1-2.qcow2" 70G -f qcow2 -o preallocation=full
+        #ledgers
         qemu-img create "${path}/worker1-3.qcow2" 600G -f qcow2 -o preallocation=full
         qemu-img create "${path}/worker1-4.qcow2" 600G -f qcow2 -o preallocation=full
         # small volumes
@@ -84,9 +85,9 @@ function staticVolumes() {
 
     #worker 2
         #journals
-        qemu-img create "${path}/worker2-1.qcow2" 75G -f qcow2 -o preallocation=full
-        qemu-img create "${path}/worker2-2.qcow2" 75G -f qcow2 -o preallocation=full
-        #journals
+        qemu-img create "${path}/worker2-1.qcow2" 70G -f qcow2 -o preallocation=full
+        qemu-img create "${path}/worker2-2.qcow2" 70G -f qcow2 -o preallocation=full
+        #ledgers
         qemu-img create "${path}/worker2-3.qcow2" 600G -f qcow2 -o preallocation=full
         qemu-img create "${path}/worker2-4.qcow2" 600G -f qcow2 -o preallocation=full
         # small volumes
@@ -97,26 +98,14 @@ function staticVolumes() {
         qemu-img create "${path}/worker3-1.qcow2" ${smallVolSz} -f qcow2 -o preallocation=full
         qemu-img create "${path}/worker3-2.qcow2" ${smallVolSz} -f qcow2 -o preallocation=full
         qemu-img create "${path}/worker3-3.qcow2" ${smallVolSz} -f qcow2 -o preallocation=full
+        qemu-img create "${path}/worker3-4.qcow2" ${verySmallVolSz} -f qcow2 -o preallocation=full
+        qemu-img create "${path}/worker3-5.qcow2" ${verySmallVolSz} -f qcow2 -o preallocation=full
+        qemu-img create "${path}/worker3-6.qcow2" ${verySmallVolSz} -f qcow2 -o preallocation=full
 
         qemu-img create "${path}/worker4-1.qcow2" ${smallVolSz} -f qcow2 -o preallocation=full
         qemu-img create "${path}/worker4-2.qcow2" ${smallVolSz} -f qcow2 -o preallocation=full
         qemu-img create "${path}/worker4-3.qcow2" ${smallVolSz} -f qcow2 -o preallocation=full
-
-
-    # for i in `eval echo {$workers_from..$workers_to}`;
-    # do
-    #     for j in `eval echo {1..$index}`;
-    #     do
-    #         echo "$path/worker-$i-$j.qcow2"
-    #         qemu-img create "$path/worker-$i-$j.qcow2" 100G -f qcow2 -o preallocation=full
-    #     done
-
-    #     for j in `eval echo {$(( $index + 1 ))..6}`;
-    #     do
-    #         echo "$path/worker-$i-$j.qcow2"
-    #         qemu-img create "$path/worker-$i-$j.qcow2" 5G -f qcow2 -o preallocation=full
-    #     done
-    # done
+        qemu-img create "${path}/worker4-4.qcow2" ${verySmallVolSz} -f qcow2 -o preallocation=full
+        qemu-img create "${path}/worker4-5.qcow2" ${verySmallVolSz} -f qcow2 -o preallocation=full
+        qemu-img create "${path}/worker4-6.qcow2" ${verySmallVolSz} -f qcow2 -o preallocation=full
 }
-
-# create $1 $2 $3 $4
